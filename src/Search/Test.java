8df63ca4640d7109718325960fb76eb13d00e230
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-08-11 13:22:32 +0200

    rozne sortowania

--- src/Search/Test.java
+++ src/Search/Test.java
@@ -6,5 +6,39 @@ public class Test {
         LinearSearch test1 = new LinearSearch();
         System.out.println(test1.search(17, ints));
         System.out.println(test1.search(0, ints));
+        System.out.println(middleElement(ints));
+        System.out.println();
+        toIndex(ints, 6);
+        System.out.println();
+        fromIndex(ints, 4);
+
+
+    }
+
+    public static Integer middleElement (Integer[] array){
+        if (array.length % 2 == 0){
+            return array[array.length/2 - 1];
+        }
+        return array[array.length/2];
+    }
+
+    public static void toIndex (Integer[] array, int index){
+        if (index < array.length) {
+            for (int i = 0; i < index; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } else {
+            System.out.println("Błędny index");
+        }
+    }
+
+    public static void fromIndex (Integer[] array, int index){
+        if (index < array.length) {
+            for (int i = index; i < array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } else {
+            System.out.println("Błędny index");
+        }
     }
 }
