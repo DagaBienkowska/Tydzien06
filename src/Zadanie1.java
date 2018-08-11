ff2f0a91d96146d2e28b760979d140044bd74214
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-08-11 10:47:18 +0200

    poprawione

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -4,29 +4,36 @@ public class Zadanie1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, List<String>> namesWithJob = new HashMap<>();
-        for (int i = 0; i < 6; i++) {
-            System.out.println("Podaj imię");
-            String name = scanner.nextLine();
-            System.out.println("Podaj dział");
-            String job = scanner.nextLine();
+        boolean boo = true;
 
+        while (true) {
+
+            System.out.println("Podaj imię i dział");
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("list")) {
+                System.out.println(namesWithJob);
+                boo = false;
+                break;
+            }
+            String[] worker = input.split(" ");
             List<String> names = new ArrayList<>();
-            if (namesWithJob.containsKey(job)){
+            if (!namesWithJob.containsKey(worker[1])) {
+                names.add(worker[0]);
+                namesWithJob.put(worker[1], names);
+            } else {
                 List<String> namesList = new ArrayList<>();
-                List<String> temp = namesWithJob.get(job);
-                for (String s : temp){
+                List<String> temp = namesWithJob.get(worker[1]);
+                for (String s : temp) {
                     namesList.add(s);
                 }
-                namesList.add(name);
-                namesWithJob.put(job, namesList);
-            } else {
-                names.add(name);
-                namesWithJob.put(job, names);
+                namesList.add(worker[0]);
+                namesWithJob.put(worker[1], namesList);
             }
 
 
         }
-        System.out.println(namesWithJob);
+
+
     }
 
 }
